7a174735ee867db26f346742df1db06d55fd0475
C00205926 <c00205926@itcarlow.ie>
2020-10-09 11:36:37 +0100

    first push

---
+++ RWM-P0-C00205926-Combined/Assets/Tests/CollatzFilterTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+  public class CollatzFilterTest
+  {
+    [Test]
+    public void CollatzFilterT()
+    {
+      int[] input = { 3, 2 ,8, 9 , 10, 105};
+      int[] output = CollatzFilter.collatz(input); //CubeFilter.cubes(input);
+      int[] expected = { 10, 1, 4, 28, 5, 316};
+      
+      CollectionAssert.AreEqual(expected, output);
+    }
+  }
+}
